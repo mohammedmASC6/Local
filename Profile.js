87e9adae816d6d9e0c3c5687e1e921232d70cf59
Mohammed Mamun <52749689+mohammedmASC6@users.noreply.github.com>
2023-12-18 00:33:59 -0500

    Update Profile.js

--- Profile.js
+++ Profile.js
@@ -1,13 +1,12 @@
 // src/components/Home.js
 import React from 'react';
 import Sidebar from './Sidebar';
-import './Sidebar.css'; // Import the CSS file
 
 const Profile = () => (
   <div className="home-container">
     <Sidebar />
     <div className="content">
-      <h2>HOME</h2>
+      <h2>PROFILE</h2>
       {/* Your home page content goes here */}
     </div>
   </div>
