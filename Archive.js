7d095e93fb5660c9ab44f405cf5fe0d4542a37e1
Mohammed Mamun <52749689+mohammedmASC6@users.noreply.github.com>
2023-12-18 00:34:31 -0500

    Update Archive.js

---
+++ Archive.js
@@ -0,0 +1,14 @@
+// Home.js
+import React from 'react';
+import Layout from './Layout';
+
+const Archive = () => (
+  <Layout>
+    <div>
+      {/* Home content goes here */}
+      <h1>ARCHIVE</h1>
+    </div>
+  </Layout>
+);
+
+export default Archive;
